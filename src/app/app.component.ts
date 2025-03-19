c0f8bc1abb0e44afbd53b2df86a3d8f4528b94d0
Sumi Kumari <sumiK9784@gmail.com>
2025-03-19 10:09:20 +0530

    Updated feature

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+  count=0
+
+  handleIncrement(){
+    this.count=this.count+1
+  }
+
+  handleDecrement(){
+    this.count=this.count-1
+  }
+
+  handleReset(){
+    this.count=0
+  }
+
+  handleCounter(val:string){
+    if(val=='minus'){
+      this.count=this.count-1
+    }else if(val=='plus'){
+      this.count=this.count+1
+    }else{
+      this.count=0
+    }
+  }
+}
